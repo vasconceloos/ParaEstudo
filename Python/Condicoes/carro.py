5e4348c0f908bb305cb03336b18e4f3a93f52ee5
Ayo <alvesayo@outlook.com>
2020-11-17 21:20:05 -0300

    Condição
    Condição if/else

---
+++ Python/Condicoes/carro.py
@@ -0,0 +1,6 @@
+tempo = int(input('Quantos anos seu corro tem? '))
+if (tempo <= 3):
+    print('Carro novo')
+else:
+    print('Carro velho')
+print('--FIM--')
---
+++ Python/Condicoes/qualeoSeuNome.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Ayo':
+  print('Que nome diferente você tem!!!')
+else:
+  print('Seu nome é tão normal!')
+print('Boa noite, {}!'.format(nome))
